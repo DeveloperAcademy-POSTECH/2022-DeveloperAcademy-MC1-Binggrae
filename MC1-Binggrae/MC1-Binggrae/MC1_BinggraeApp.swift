939427154f9af3c684724e3cd9cfc529e468b86b
insub kim <vb15@kakao.com>
2022-04-06 16:18:58 +0900

    init

---
+++ MC1-Binggrae/MC1-Binggrae/MC1_BinggraeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC1_BinggraeApp.swift
+//  MC1-Binggrae
+//
+//  Created by Kim Insub on 2022/04/06.
+//
+
+import SwiftUI
+
+@main
+struct MC1_BinggraeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MC1-Binggrae/MC1-Binggrae/Views/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MC1-Binggrae
+//
+//  Created by Kim Insub on 2022/04/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .font(.custom("NotoSansCJKkr-Regular", size: 28))
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
